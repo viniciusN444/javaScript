ee4cd49780d5945104aa9449b70fc07a4f068d93
Vinicius Oliveira <157658093+viniciusN444@users.noreply.github.com>
2024-07-22 21:23:54 -0300

    Condição Simples e Composta em JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,14 @@
+/*
+    Quando so usado o IF sem o ELSE se chama CONDIÇÃO SIMPLES
+    Quando é usado IF E ELSE se chama CONDIÇÃO COMPOSTA!
+*/
+
+var vel = 78.2;
+
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade permirida, VOCÊ ESTÁ MULTADO!');
+}else{
+    console.log(`Dirija sempre usando cinto de segurança!`);  
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+//Condição Composta
+var pais = 'Alemanha';
+console.log(`Vivendo em ${pais}`);
+
+if(pais == 'Brasil'){
+    console.log('Você é brasileiro!')
+}else{
+    console.log('Você é estrangeiro!')
+}
