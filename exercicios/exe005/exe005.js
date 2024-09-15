f3553c222dfd8567ecd4ce7e6c17cee7c47b1e83
grupotcc3h3 <157658093+viniciusN444@users.noreply.github.com>
2024-09-14 21:48:33 -0300

    Ajustes

--- exercicios/exe005/exe005.js
+++ exercicios/exe005/exe005.js
@@ -1,25 +1,33 @@
-function Carregar(){
-    var mensagem = document.getElementById("msg")
-    var comprimentar = document.getElementById("comprimentar")
-    var img = document.getElementById("imagem")
-    var data =  new Date();
-    var hora = data.getHours()
+function Carregar() {
+    // Obtendo os elementos do DOM
+    var mensagem = document.getElementById("msg");       // Elemento onde a mensagem será exibida
+    var comprimentar = document.getElementById("comprimentar"); // Elemento onde a saudação será exibida
+    var img = document.getElementById("imagem");        // Elemento onde a imagem de fundo será alterada
     
-   if (hora >= 0){
-    comprimentar.innerHTML = "Boa madrugada!"
-    mensagem.innerHTML = `Agora são ${hora} horas da madruga`;
-    img.src = '../imgs_exercicios/noite (1).png'
-   } else if (hora >= 6 && hora <= 12){
-    comprimentar.innerHTML = "Bom diaa!"
-    mensagem.innerHTML = `Agora são ${hora} horas da manhã`;
-    img.src = '../imgs_exercicios/img_manha (1).png'
-   }if(hora <= 18){
-    comprimentar.innerHTML = "Boa Tarde"
-    mensagem.innerHTML = `Agora são ${hora} horas da tarde!`
-    img.src = '../imgs_exercicios/img_tarde (1).png'
-   }else{
-    comprimentar.innerHTML = "Boa Noite!"
-    mensagem.innerHTML = `Agora são ${hora} horas da noite!`
-    img.src = '../imgs_exercicios/img_noite (1).png'
-   }
+    // Criando um objeto Date para obter a hora atual
+    var data = new Date();
+    var hora = data.getHours();  // Obtendo a hora atual do sistema
+
+    // Definindo o fundo e a mensagem de acordo com a hora
+    if (hora >= 0 && hora < 6) {
+        // Madrugada: 00:00 a 05:59
+        comprimentar.innerHTML = "Boa madrugada!";  // Saudação para a madrugada
+        mensagem.innerHTML = `Agora são ${hora} horas da madrugada.`; // Mensagem mostrando a hora
+        img.src = '../imgs_exercicios/noite.png'; // Imagem para a madrugada
+    } else if (hora >= 6 && hora < 12) {
+        // Manhã: 06:00 a 11:59
+        comprimentar.innerHTML = "Bom dia!";  // Saudação para a manhã
+        mensagem.innerHTML = `Agora são ${hora} horas da manhã.`; // Mensagem mostrando a hora
+        img.src = '../imgs_exercicios/img_manha (1).png'; // Imagem para a manhã
+    } else if (hora >= 12 && hora < 19) {
+        // Tarde: 12:00 a 18:59
+        comprimentar.innerHTML = "Boa tarde!";  // Saudação para a tarde
+        mensagem.innerHTML = `Agora são ${hora} horas da tarde.`; // Mensagem mostrando a hora
+        img.src = '../imgs_exercicios/img_tarde (1).png'; // Imagem para a tarde
+    } else {
+        // Noite: 19:00 a 23:59
+        comprimentar.innerHTML = "Boa noite!";  // Saudação para a noite
+        mensagem.innerHTML = `Agora são ${hora} horas da noite.`; // Mensagem mostrando a hora
+        img.src = '../imgs_exercicios/img_noite (1).png'; // Imagem para a noite
+    }
 }
