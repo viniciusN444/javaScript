818c05b28548238f3dfca3c926f7bf833e803b10
GrupoTCC3h3 <grupodstcc@gmail.com>
2024-12-03 21:29:34 -0300

    Exercício da tabuada junto com o professor.

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-window.alert("Olá, Mundo!")
+function tabuada(){
+    var numeroDigitado = window.document.querySelector('#numeroTabuada').value;
+    var sessaoTabuada = window.document.getElementById('seltab')
+
+    if(numeroDigitado.length == 0){
+        alert("[ERRO] Digite o número que deseja ver a tabuada!")
+    }else{
+        var numConvertido = Number(numeroDigitado); // conversão do numero digitado que antes era uma string.
+        var contador = 1;
+        sessaoTabuada.innerHTML = '';
+        while (contador <= 10){
+            var item = document.createElement('option');
+            item.text = `${numConvertido} X ${contador} = ${numConvertido * contador}`
+            item.value = `sessaoTabuada${contador}`; // valores do option
+            contador++
+            sessaoTabuada.appendChild(item);
+        }
+    }
+}
