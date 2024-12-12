4ee3f241e72ec5753addbdb46707703f16b51ceb
GrupoTCC3h3 <grupodstcc@gmail.com>
2024-12-11 21:42:35 -0300

    Dando inicio ao exercicio com array e funções com parametros.

---
+++ exercicios/analisador_de_numeros/script.js
@@ -0,0 +1,16 @@
+function adicionar(numeros){
+    let numeroDigitado = window.document.querySelector('#numero').value;
+    if(numeroDigitado.length == 0){
+        alert("[ERRO] Por favor, insira um número de 1 à 100!")
+    }
+
+    let numCovertido = Number(numeroDigitado);
+
+    numeros = [];
+
+    numCovertido = numeros;
+    for(let c; numCovertido.length < c; c++){
+        console.log(c[numCovertido])
+        //voltarei
+    }
+}
